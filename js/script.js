d9bef9e3168c11c5e4b378c2e2fa8be4a442b271
Julio Yael Cárdenas Carrera <145951094+JulioYCardenas@users.noreply.github.com>
2026-05-09 14:04:10 -0600

    Enhance menu icon interactions on hover
    Updated folder and caret icons on mouse enter and leave events.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,37 @@ const menus = document.querySelectorAll('.menu-know');
 
 menus.forEach(menu => {
 
-    const icono = menu.querySelector('.folder-icon');
-
+    const folderIcon = menu.querySelector('.folder-icon');
+    const caretIcon = menu.querySelector('.caret');
+    caretIcon.style.color = 'rgb(177, 177, 177)';
+    folderIcon.style.color = 'rgb(0, 217, 255)';
     menu.addEventListener('mouseenter', () => {
-        icono.classList.remove('fa-folder');
-        icono.classList.add('fa-folder-open');
+
+        /* Folder */
+
+        folderIcon.classList.remove('fa-folder');
+        folderIcon.classList.add('fa-folder-open');
+
+        /* Caret */
+
+        caretIcon.classList.remove('fa-caret-right');
+        caretIcon.classList.add('fa-caret-down');
+
     });
 
     menu.addEventListener('mouseleave', () => {
-        icono.classList.remove('fa-folder-open');
-        icono.classList.add('fa-folder');
+
+        /* Folder */
+
+        folderIcon.classList.remove('fa-folder-open');
+        folderIcon.classList.add('fa-folder');
+
+        /* Caret */
+
+        caretIcon.classList.remove('fa-caret-down');
+        caretIcon.classList.add('fa-caret-right');
+
+        
     });
 
 });
